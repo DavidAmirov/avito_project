313d57f0393a96b8b6d62eb75e7f9508ec041a60
Давид Амиров <david.talish@yandex.ru>
2023-02-04 11:17:37 +0300

    Создал приложение main, написал 3 шаблона: базовый, главная страница, прочие страницы. Написал к ним пути и контроллеры.

---
+++ avito/avito/urls.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.contrib.staticfiles.views import serve
+from django.views.decorators.cache import never_cache
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('main.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns.append(path('static/<path:path>', never_cache(serve)))
